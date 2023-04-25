eff68093ebd5e31c1e32b54f1b4fff7b36c0ea65
Abhiraj R <113856741+4bhy@users.noreply.github.com>
2023-04-25 09:00:40 +0530

    _redirects

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -171,10 +171,10 @@ const LoginScreen = () => {
                                                     <svg
                                                         onClick={() => setShowPassword(true)}
                                                         xmlns="http://www.w3.org/2000/svg"
-                                                        class="h-4 w-4 text-gray-400"
+                                                        class={showPassword ? 'h-4 w-4 text-gray-700' : 'h-4 w-4 text-gray-300'}
                                                         fill="none"
                                                         viewBox="0 0 24 24"
-                                                        stroke="currentColor"
+                                                        stroke="currentColor"   
                                                     >
                                                         <path
                                                             stroke-linecap="round"
@@ -207,7 +207,8 @@ const LoginScreen = () => {
                                                 <span onClick={() => setShowPassword(true)} class="absolute inset-y-0 end-0 grid place-content-center px-4">
                                                     <svg
                                                         xmlns="http://www.w3.org/2000/svg"
-                                                        class="h-4 w-4 text-gray-400"
+                                                        class={showPassword ? 'h-4 w-4 text-gray-700' : 'h-4 w-4 text-gray-300'}
+
                                                         fill="none"
                                                         viewBox="0 0 24 24"
                                                         stroke="currentColor"
@@ -270,7 +271,8 @@ const LoginScreen = () => {
                                                 <span class="absolute inset-y-0 end-0 grid place-content-center px-4">
                                                     <svg
                                                         xmlns="http://www.w3.org/2000/svg"
-                                                        class="h-4 w-4 text-gray-400"
+                                                        class={showPassword ? 'h-4 w-4 text-gray-700' : 'h-4 w-4 text-gray-300'}
+
                                                         fill="none"
                                                         viewBox="0 0 24 24"
                                                         stroke="currentColor"
